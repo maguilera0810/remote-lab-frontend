1e6c138a8453c5ee491980e06d7e08471b5819cb
Mauricio Aguilera <mauricio@familify.com>
2023-08-12 10:49:19 -0500

    datatable

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,21 +1,13 @@
 import React from 'react';
-import Sidebar from '../components/organisms/SideBar';
-import { SidebarProps } from '../interfaces/NavigationIntefaces';
-import { userData } from '../data/UserData';
-import { menuItems } from '../data/MenuData';
+import DataTable from '../components/organisms/tables/DataTable';
+import GenericTemplate from '../components/templates/GenericTemplate';
 
 const Home: React.FC = () => {
-  const sidebar: SidebarProps = {
-    user: {
-      name: "Mauricio",
-      avatarUrl: "",
-    },
-    menuItems: menuItems,
-  }
-
   return <>
-    <Sidebar user={sidebar.user} menuItems={sidebar.menuItems} />
-    <h2>Home Page!</h2>
+    <GenericTemplate>
+      <h2>Home Page!</h2>
+      <DataTable></DataTable>
+    </GenericTemplate>
   </>;
 };
 
